887cf435ad1c39982cb80a2b7f730e2029fc294e
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2023-10-17 22:45:57 +0100

    First try

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hello via Bun!')
+/* eslint-disable space-before-function-paren */
+
+import { serve, file } from 'bun'
+
+const server = serve({
+  port: 3000,
+  fetch(req) {
+    const url = new URL(req.url)
+    console.log('--')
+    console.log(req.method, url.pathname)
+
+    if (url.pathname === '/') {
+      return new Response(file('./src/index.html'))
+    }
+
+    return new Response('Hello World!')
+  },
+  error(error) {
+    return new Response(error)
+  }
+})
+
+console.log(`Server running at http://localhost:${server.port}`)
